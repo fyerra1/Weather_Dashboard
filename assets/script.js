2740a87a3a3e8982b4ac4e06e1d6b45e2d4ff95b
Franky Erra <frankyerra43@gmail.com>
2022-05-22 23:10:37 -0700

    added code to grab coordinates based on city/user input

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,8 @@ var fetchButton = document.getElementById('search-btn');
 
 function getApi() {
   // fetch request gets a list of all the repos for the node.js organization
-  var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + '&limit=1&appid=' + apiKey;
+  var userInput = document.getElementById('location-search').value;
+  var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + userInput + '&limit=1&appid=' + apiKey;
   console.log(requestUrl);
   fetch(requestUrl)
     .then(function (response) {
@@ -14,23 +15,11 @@ function getApi() {
     })
     .then(function (data) {
       console.log(data)
-      //Loop over the data to generate a table, each table row will have a link to the repo url
-      // for (var i = 0; i < data.length; i++) {
-      //   // Creating elements, tablerow, tabledata, and anchor
-      //   var createTableRow = document.createElement('tr');
-      //   var tableData = document.createElement('td');
-      //   var link = document.createElement('a');
 
-      //   // Setting the text of link and the href of the link
-      //   link.textContent = data[i].html_url;
-      //   link.href = data[i].html_url;
-
-      //   // Appending the link to the tabledata and then appending the tabledata to the tablerow
-      //   // The tablerow then gets appended to the tablebody
-      //   tableData.appendChild(link);
-      //   createTableRow.appendChild(tableData);
-      //   tableBody.appendChild(createTableRow);
-      // }
+      var lon = data[0].lon
+      var lat = data[0].lat
+      console.log(lon);
+      console.log(lat);
     });
 }
 
