86ce5d954e093f536b827c1ea0a66e01e92f21cc
Franky Erra <frankyerra43@gmail.com>
2022-05-26 14:28:42 -0700

    modified api links

--- assets/script.js
+++ assets/script.js
@@ -8,7 +8,7 @@ searchButton.addEventListener('click', getApi)
 function getApi() {
   // fetch request gets coordinates based on city/user input
   var userInput = document.getElementById('location-search').value;
-  var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + userInput + '&appid=' + apiKey;
+  var requestUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + userInput + '&appid=' + apiKey;
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
@@ -51,9 +51,10 @@ function renderCurrent(data){
   var timeId = document.getElementById('current-day')
   timeId.textContent = '(' + (today.format("M/D/YYYY")) + ')';
 
+  console.log(data.current);
   var iconId = document.getElementById('weather-icon');
   var weatherIcon = data.current.weather[0].icon;
-  iconId.src = 'http://openweathermap.org/img/w/' + weatherIcon + '.png';
+  iconId.src = 'https://openweathermap.org/img/w/' + weatherIcon + '.png';
   console.log(data);
 
   var tempData = data.current.temp;
@@ -89,7 +90,7 @@ function renderFuture(data){
 
     var displayIcon1 = document.getElementById('icon' + i)
     var futureIcon = data.daily[i].weather[0].icon;
-    displayIcon1.src = 'http://openweathermap.org/img/w/' + futureIcon + '.png';
+    displayIcon1.src = 'https://openweathermap.org/img/w/' + futureIcon + '.png';
 
     var displayTemp1 = document.getElementById('temp' + i)
     var futureTemp = data.daily[i].temp.max;
@@ -116,7 +117,7 @@ function searchHistory(city){
   cityBtn.addEventListener('click', function(e){
   var selectedCity = e.target.textContent;
 
-  var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + selectedCity + '&appid=' + apiKey;
+  var requestUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + selectedCity + '&appid=' + apiKey;
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
@@ -163,7 +164,7 @@ function renderStorage (){
   var selectedCity = e.target.textContent;
   console.log(selectedCity);
 
-  var requestUrl = 'http://api.openweathermap.org/geo/1.0/direct?q=' + selectedCity + '&appid=' + apiKey;
+  var requestUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' + selectedCity + '&appid=' + apiKey;
   fetch(requestUrl)
     .then(function (response) {
       return response.json();
